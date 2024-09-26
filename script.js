e97814fa42baee1e4fda4300859461c3b6b71219
Manikanta8887 <swamimanikanta66@gmail.com>
2024-09-26 22:11:52 +0530

    Adding Files

---
+++ script.js
@@ -0,0 +1,19 @@
+
+// Primitive DataTypes
+
+var name="Manikanta";
+var age=22;
+var degree="B.Tech";
+var hobbies=undefined;
+var exercise=null;
+
+console.log(name);
+console.log(age);
+console.log(degree);
+console.log(true);
+console.log(false);
+
+let a=10;
+console.log(a);
+const b=20;
+console.log(b);
